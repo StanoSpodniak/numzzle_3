3d61582620dda9c831f4bf86c6baa4ab77ea6817
StanoSpodniak <stano28@gmail.com>
2025-03-28 23:27:41 +0100

    Added responsitivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import styles from "./Header.module.css";
 function Header() {
     return (
         <div className={styles.headerContainer}>
-            <h1 className={styles.title}>Numzzle</h1>
+            <a href="/">
+                <h1 className={styles.title}>Numzzle</h1>
+            </a>
         </div>
     );
 }
