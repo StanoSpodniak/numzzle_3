73c8d6436348296a88417c83023b2f03c0bd8c2e
StanoSpodniak <stano28@gmail.com>
2025-03-28 23:42:50 +0100

    Improved responsivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,7 @@ import styles from "./Header.module.css";
 function Header() {
     return (
         <div className={styles.headerContainer}>
-            <a href="/">
-                <h1 className={styles.title}>Numzzle</h1>
-            </a>
+            <h1 className={styles.title}>Numzzle</h1>
         </div>
     );
 }
