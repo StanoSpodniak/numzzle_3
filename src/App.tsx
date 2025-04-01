3305e39e01f9cdcfec3d15215dccc3ce46ce57fd
StanoSpodniak <stano28@gmail.com>
2025-04-01 22:35:49 +0200

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Game from "./components/Game";
 import Description from "./components/Description";
+import Footer from "./components/Footer";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Header />
             <Game />
             <Description />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <div className={styles.footerContainer}>
+            <p>Made in 2025 by Stano28</p>
+        </div>
+    );
+}
+
+export default Footer;
